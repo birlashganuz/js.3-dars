e100becc9ee930b2622954152636b99a2c91dec3
birlashganuz <birlashganuz@gmail.com>
2024-02-21 23:40:33 +0500

    21.02.2024:vazifa

---
+++ scripts/functions.js
@@ -0,0 +1,30 @@
+n = +prompt('Yulduzchalar sonini kiriting:')
+while (isNaN(n)) {
+    n = +prompt('Yulduzchalar sonini natural sonda kiriting:')
+}
+if (n % 2 == 1) {
+    n += 1
+}
+
+let star = prompt('Belgilangan katakchalarga kiritiladigan belgi:', '*')
+while (star.length > 1) {
+    star = prompt('Iltimos faqat 1 ta belgi kiriting:', '*')
+}
+
+function stars(n = 3) {
+    console.log(`Siz kiritgan ma\'lumot asosida ${n + 1}x${n + 1} kvadrat shakllantirildi.`)
+    let space = '( )'
+    star = '(' + star + ')'
+    for (let i = 0; i <= n; i++) {
+        let row = '';
+        for (let j = 0; j <= n; j++) {
+            if ((i == 0) || (j == 0) || (i == n) || (j == n) || (i == j) || ((i + j) == n)) {
+                row += star
+            } else { row += space }
+        }
+        console.log(row)
+    }
+    alert('Natija console da berildi.')
+}
+
+stars(n)
