1aaefbc0480a835104f71939bf10dcd14c452025
birlashganuz <birlashganuz@gmail.com>
2024-02-24 17:43:22 +0500

    Sanoq sistema

--- scripts/ex-9.js
+++ scripts/ex-9.js
@@ -1,27 +1,48 @@
 function pass(a, n) {
-    let b = a
+    // let b = a
     let result = ''
-    while (a >= n) {
+    if (n > 1 && n <= 10) {
+        while (a >= n) {
+            result = String(a % n) + result
+            a = (a - (a % n)) / n
+        }
+        result = String(a) + result
+    } else if (n >= 10 && n < 17) {
+        let k = {
+            10: 'A',
+            11: 'B',
+            12: 'C',
+            13: 'D',
+            14: 'E',
+            15: 'F'
+        }
 
-        if (n == 16) {
-            rest = a % n
-            if (rest == 10) { s_rest = 'A' }
-            else if (rest == 11) { s_rest = 'B' }
-            else if (rest == 12) { s_rest = 'C' }
-            else if (rest == 13) { s_rest = 'D' }
-            else if (rest == 14) { s_rest = 'E' }
-            else if (rest == 15) { s_rest = 'R' }
-            result = s_rest + result
-        } else { result = String(a % n) + result }
+        while ((a > n && a != n) || (a == n)) {
+            let rest = a % n
+            if (rest > 9 && rest < 16) {
+                result = k[rest] + result
+            } else {
+                result = String(a % n) + result
+            }
+            a = (a - (a % n)) / n
+        }
 
-        a = (a - (a % n)) / n
+        if (a > 9 && a < 16) {
+            result = k[a] + result
+        } else {
+            result = String(a) + result
+        }
     }
-    result = String(a) + result
     return result
 }
-a = +prompt('a ni kiriting')
-n = +prompt('n ni kiriting')
+let a = +prompt('a ni kiriting')
+while (isNaN(a)) {
+    a = +prompt('a ni kiritishda xatolik bor.')
+}
+let n = +prompt('n ni kiriting')
+while (isNaN(n) || n > 16) {
+    n = +prompt('n ni kiritishda xatolik bor. ( 1 < n < 17 )')
+}
 result = pass(a, n)
-// console.log(Number(a).toString(16))
-alert(result)
-
+// console.log(Number(a).toString(n))
+alert(`${a}(10) => ${result}(${n})`)
